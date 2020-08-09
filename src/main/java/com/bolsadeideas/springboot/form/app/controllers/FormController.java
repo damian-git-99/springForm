413deca5a05e9ef108df35ca989c73b2a0c630fe
Damian-Escritorio <damiangalvan66@gmail.com>
2020-08-09 13:46:48 -0500

    Model object creado y validator agregado

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,10 +1,18 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
 public class FormController {
@@ -14,20 +22,29 @@ public class FormController {
 		model.addAttribute("titulo", "Form");
 		return "form";
 	}
-	
+
 	@PostMapping("/form")
-	public String processForm(Model model, 
+	public String processForm(@Valid Usuario user, BindingResult result , Model model,
 			@RequestParam(name = "username", defaultValue = "") String username,
 			@RequestParam(name = "password", defaultValue = "") String password,
 			@RequestParam(name = "email", defaultValue = "") String email) {
-			
-			model.addAttribute("titulo", "Form Result");
-			model.addAttribute("username", username);
-			model.addAttribute("password", password);
-			model.addAttribute("email", email);
-			
-			return "results";
+		
+		model.addAttribute("titulo", "Form Result");
+		if (result.hasErrors()) {
+			Map<String, String> errores = new HashMap<>();
+			result.getFieldErrors().forEach(err -> {
+				errores.put(err.getField(), "El campo "
+						.concat(err.getField())
+						.concat(" ")
+						.concat(err.getDefaultMessage()));
+			});
+			model.addAttribute("error", errores);
+			return "form";
+		}
+		
+		
+		model.addAttribute("user", user);
+		return "results";
 	}
-	
-	
+
 }
